8e0c8d21391bc8eb4abe7f4917bc779fee0a8dd5
MemaroX <boy15maher50@gmail.com>
2024-08-07 16:21:52 +0300

    Add project files.

---
+++ Controllers/InstructorController.cs
@@ -0,0 +1,23 @@
+﻿using ASPDAY2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASPDAY2.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private readonly ItiContext itiContext;
+
+        public InstructorController(ItiContext context)
+        {
+            itiContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            ICollection<Instructor> instructors = itiContext.Instructors.Take(10).ToList();
+            return View(instructors);
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using ASPDAY2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDAY2.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ItiContext itiContext;
+        public StudentController(ItiContext context)
+        {
+            itiContext = context;
+        }
+        public IActionResult Index()
+        {
+            ICollection<Student> students = itiContext.Students.Take(10).ToList();
+            return View(students);
+        }
+
+
+    }
+}
